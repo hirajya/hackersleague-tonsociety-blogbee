a335e39519ad16aa00c8c64ca6ca55ad0c339100
Danyssa B. Tamayo <120694207+tamayodb@users.noreply.github.com>
2024-11-17 00:38:02 +0800

    draft

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,20 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      tangina mo
+    <div class="w-[350px] h-[146px] justify-center items-center inline-flex">
+    <div class="w-[350px] h-[146px] relative">
+        <div class="w-[321px] left-0 top-[30px] absolute text-white text-xl font-bold font-['DM Sans'] leading-tight">The 5 paid subscriptions I actually use in 2024 as a software engineer</div>
+        <img class="w-[89px] h-[49.68px] left-[261px] top-[30px] absolute" src="https://via.placeholder.com/89x50" />
+        <div class="left-[1px] top-[94px] absolute text-[#969696] text-sm font-normal font-['DM Sans'] leading-[13.41px]">Tools I use that are cheaper than Netflix</div>
+        <div class="left-[1px] top-[94px] absolute text-[#969696] text-sm font-normal font-['DM Sans'] leading-[13.41px]">Tools I use that are cheaper than Netflix</div>
+        <div class="left-[32px] top-[130px] absolute text-[#d9d9d9] text-sm font-normal font-['DM Sans'] leading-[13.41px]">12.3K</div>
+        <div class="left-[108px] top-[130px] absolute text-[#d9d9d9] text-sm font-normal font-['DM Sans'] leading-[13.41px]">133</div>
+        <div class="left-[249px] top-[130px] absolute text-[#888888] text-sm font-normal font-['DM Sans'] leading-[13.41px]">Jan 4, 2024</div>
+        <div class="left-[32px] top-[8.22px] absolute text-white text-[10.33px] font-light font-['DM Sans'] leading-[10.33px]">Jacob Bennett</div>
+        <div class="left-[104.18px] top-[8.22px] absolute text-white text-[10.33px] font-light font-['DM Sans'] leading-[10.33px]">in Level Up Coding</div>
+        <img class="w-6 h-6 left-0 top-0 absolute rounded-full" src="https://via.placeholder.com/24x24" />
+        <div class="w-[19px] h-[19px] left-[331px] top-[127px] absolute"></div>
     </div>
+</div>
   )
 }
