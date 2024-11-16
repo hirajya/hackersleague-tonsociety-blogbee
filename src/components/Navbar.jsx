487d650446da1811415eda0de91f58c92d988389
rajkyutiebaby <rodneyestrada2425@gmail.com>
2024-11-16 23:08:11 +0800

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Navbar() {
   return (
-    <div>
-      Navbar to
+    <div className='p-4'>
+      <Image
+              className=""
+              src="./BlogbeeLogo.svg"
+              alt="Blogbee Logo"
+              width={100}
+              height={100}
+            />
     </div>
   )
 }
